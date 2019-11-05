06976ee1113755770190a389f34b9ad90769bd8d
charmaine211 <charmaine211@users.noreply.github.com>
2019-11-05 00:42:56 +0000

    automated commit by submit50

--- resize.c
+++ resize.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include "bmp.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     // ensure proper usage
     if (argc != 4)
@@ -17,8 +17,8 @@ int main(int argc, char* argv[])
 
     // Filenames
     int n = atoi(argv[1]);
-    char* infile = argv[2];
-    char* outfile = argv[3];
+    char *infile = argv[2];
+    char *outfile = argv[3];
 
 
     if (n <= 0 || n >= 100)
@@ -31,7 +31,7 @@ int main(int argc, char* argv[])
 
 
     // Input file open
-    FILE* inptr = fopen(infile, "r");
+    FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
         printf("Could not open %s.\n", infile);
@@ -40,7 +40,7 @@ int main(int argc, char* argv[])
 
 
     // open output file
-    FILE* outptr = fopen(outfile, "w");
+    FILE *outptr = fopen(outfile, "w");
     if (outptr == NULL)
     {
         fclose(inptr);
@@ -49,17 +49,17 @@ int main(int argc, char* argv[])
     }
 
 
-    // read infile's BITMAPFILEHEADER
+    // Read infile's BITMAPFILEHEADER
     BITMAPFILEHEADER original_bf;
     fread(&original_bf, sizeof(BITMAPFILEHEADER), 1, inptr);
 
 
-    // read infile's BITMAPINFOHEADER
+    // Read infile's BITMAPINFOHEADER
     BITMAPINFOHEADER original_bi;
     fread(&original_bi, sizeof(BITMAPINFOHEADER), 1, inptr);
 
 
-    // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+    // Ensure infile is (likely) a 24-bit uncompressed BMP 4.0
     if (original_bf.bfType != 0x4d42 || original_bf.bfOffBits != 54 || original_bi.biSize != 40 || original_bi.biBitCount != 24 || original_bi.biCompression != 0)
     {
         fclose(outptr);
@@ -71,7 +71,7 @@ int main(int argc, char* argv[])
 
     // Initialize the new_bf and bi
     BITMAPFILEHEADER bf = original_bf;
-    BITMAPINFOHEADER bi= original_bi;
+    BITMAPINFOHEADER bi = original_bi;
 
 
     // Determine new height and width
@@ -80,15 +80,15 @@ int main(int argc, char* argv[])
 
 
     // Determine padding for scanlines for original en new file
-    int original_padding =  (4 - (original_bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
-    int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    int original_padding = (4 - (original_bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
 
     // Determine new image sizes for bi and bf
-    bi.biSizeImage = (bi.biWidth *sizeof(RGBTRIPLE) + padding) * abs(bi.biHeight);
+    bi.biSizeImage = (bi.biWidth * sizeof(RGBTRIPLE) + padding) * abs(bi.biHeight);
 
 
-   // 54 is BF+BI
+    // 54 is BF+BI
     bf.bfSize = bi.biSizeImage + 54;
 
 
@@ -97,29 +97,29 @@ int main(int argc, char* argv[])
     fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
 
-    // iterate over infile's scanlines
-    //changed to original_biheight to prevent confusion
+    // Iterate over infile's scanlines
+    // Changed to original_biheight to prevent confusion
     for (int i = 0, original_biHeight = abs(original_bi.biHeight); i < original_biHeight; i++)
     {
-        //Not efficient, but scans over the same input line for n * output rows
+        // Not efficient, but scans over the same input line for n * output rows
         for (int l = 0; l < n; l++)
         {
-            // iterate over pixels in scanline
+            // Iterate over pixels in scanline
             for (int j = 0; j < original_bi.biWidth; j++)
             {
-                // temporary storage
+                // Temporary storage
                 RGBTRIPLE triple;
 
 
-                // read RGB triple from infile
+                // Read RGB triple from infile
                 fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
 
-              //BGR + BGR + BGR factor # of times on width
-              for (int r = 0; r < n; r++)
-              {
-                  fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
-              }
+                // BGR + BGR + BGR factor # of times on width
+                for (int r = 0; r < n; r++)
+                {
+                    fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+                }
             }
 
 
@@ -137,7 +137,7 @@ int main(int argc, char* argv[])
             //pushes cursor back and iterate # of n times on "L" row
             if (l < n - 1)
             {
-                long offset = original_bi.biWidth * sizeof(RGBTRIPLE)+ original_padding;
+                long offset = original_bi.biWidth * sizeof(RGBTRIPLE) + original_padding;
 
 
                 // By default, files are unsigned (nonnegative).
@@ -149,13 +149,13 @@ int main(int argc, char* argv[])
     }
 
 
-    // close infile
+    // Close infile
     fclose(inptr);
 
-    // close outfile
+    // Close outfile
     fclose(outptr);
 
 
-    // that's all folks
+    // Success
     return 0;
 }
