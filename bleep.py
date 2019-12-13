966ba2a9429a9db54733021d9f7eece070e6fe4d
charmaine211 <charmaine211@users.noreply.github.com>
2019-12-13 19:48:36 +0000

    automated commit by check50 [check50=True]

--- bleep.py
+++ bleep.py
@@ -4,45 +4,47 @@ from sys import argv
 
 
 def main():
-    # Check if the user provides 2 commandline arguments
+    bleep("What message would you like to censor?\n")
+
+
+def bleep(prompt):
+    # Check if the user provides 2 command line arguments
     if len(sys.argv) != 2:
-        # Exit when user does not provide 2 commandline arguments
+        # Exit when user does not provide 2 command line arguments
         sys.exit("Usage: python bleep.py dictionary")
 
-    words = set()
+    # Variables to safe filename and words from file in set
     banned_words = argv[1]
+    words = set()
 
-
-    # Open and read file
-    # banned = open(r"File_Name","r")
+    # Open file to read
     file = open(banned_words, "r")
 
-    # list_words = banned.readlines()
-    # Turn list into set. Set deletes double words
-    # set_words = set(list_words)
+    # Read file line for line and add to set words, deleting the \n and close file
     for line in file:
         words.add(line.rstrip("\n"))
     file.close()
 
-
     # Prompt user for input: "What message would you like to censor?"
-    # Save input in word
-    sentence = get_string("What message would you like to censor?\n")
+    sentence = get_string(prompt)
 
-    # Zin in woorden opsplitsen en vergelijken met de woorden in de set words
+    # Split sentence up in a list word, and create variable to safe new redacted words
     word = sentence.split()
     redacted = ''
 
+    # For every word
     for i in range(len(word)):
-        # Check if word is in words
+        # Check if word is in set words
         if word[i].lower() in words:
-            # Add * times the length of word to line without \n
+            # If it's a banned word, add * times the length of word to redacted
             for j in range(len(word[i])):
                 redacted  = redacted + "*"
             redacted = redacted + " "
+        # Else do nothing with the word and just add it to redacted
         else:
             redacted = redacted + str(word[i]) + " "
 
+    # Print word without last space
     print(redacted[:-1])
 
 if __name__ == "__main__":
