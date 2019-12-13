f4c805046353936f2348b004c72e3b745405709a
charmaine211 <charmaine211@users.noreply.github.com>
2019-12-13 19:40:28 +0000

    automated commit by check50 [check50=True]

--- bleep.py
+++ bleep.py
@@ -4,42 +4,46 @@ from sys import argv
 
 
 def main():
-    bleep("What message would you like to censor?")
-
-def bleep(prompt):
     # Check if the user provides 2 commandline arguments
     if len(sys.argv) != 2:
         # Exit when user does not provide 2 commandline arguments
         sys.exit("Usage: python bleep.py dictionary")
 
+    words = set()
+    banned_words = argv[1]
+
+
     # Open and read file
     # banned = open(r"File_Name","r")
-    banned = open(sys.argv[2], 'r')
+    file = open(banned_words, "r")
 
     # list_words = banned.readlines()
     # Turn list into set. Set deletes double words
     # set_words = set(list_words)
-    set_words = set(banned.readlines())
+    for line in file:
+        words.add(line.rstrip("\n"))
+    file.close()
+
 
     # Prompt user for input: "What message would you like to censor?"
     # Save input in word
-    sentence = get_string(prompt)
+    sentence = get_string("What message would you like to censor?\n")
 
-    # Test
-    print(sentence)
-    print(set_words)
+    # Zin in woorden opsplitsen en vergelijken met de woorden in de set words
+    word = sentence.split()
+    redacted = ''
 
-    # Check if word is in set_words
-        # If word exists in set_words
+    for i in range(len(word)):
+        # Check if word is in words
+        if word[i].lower() in words:
             # Add * times the length of word to line without \n
-        # Else
-            # Add word to line without \n
-    # Add \n to the end of line and print
+            for j in range(len(word[i])):
+                redacted  = redacted + "*"
+            redacted = redacted + " "
+        else:
+            redacted = redacted + str(word[i]) + " "
 
-    # Close file
-    banned.close()
-
-    return sentence
+    print(redacted[:-1])
 
 if __name__ == "__main__":
     main()
