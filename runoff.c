1c23cc49ede93a78c35d74ef222f885d91470612
charmaine211 <charmaine211@users.noreply.github.com>
2020-01-22 14:14:10 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -168,18 +168,20 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int tie_breaker = (candidate_count / 2) + 1;
+    candidate winner;
+    winner.votes = candidate_count / 2;
 
     for (int l = 0; l < candidate_count; l++)
     {
-        if (!candidates[l].eliminated && candidates[l].votes > tie_breaker)
+        if (!candidates[l].eliminated && candidates[l].votes > winner.votes)
         {
-            printf("%s", candidates[l].name);
-            return true;
+            winner.name = candidates[l].name;
+            winner.votes = candidates[l].votes;
         }
     }
 
-    return false;
+    printf("%s", winner.name);
+    return true;
 }
 
 // Return the minimum number of votes any remaining candidate has
